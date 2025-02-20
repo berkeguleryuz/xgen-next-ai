08640acd71febc39afd9684ded163603fe9a227e
Berke Güleryüz <berkeguleryuz@gmail.com>
2025-02-20 23:42:48 +0100

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
+import HeroSection from "@/components/HeroSection";
+import UserInput from "@/components/home/UserInput";
+import UserOutput from "@/components/home/UserOutput";
+
 export default function Home() {
   return (
-    <main className="relative h-full w-full min-h-screen text-white">
-      <div className="text-4xl font-bold text-center justify-center flex items-center h-full min-h-screen">Hello</div>
+    <main className="relative h-full w-full text-white">
+      <HeroSection />
+      <div className="grid grid-cols-2 gap-24 container mx-auto">
+        <UserInput />
+        <UserOutput />
+      </div>
     </main>
   );
 }
---
+++ components/home/UserInput.tsx
@@ -0,0 +1,90 @@
+"use client";
+import React from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Separator } from "../ui/separator";
+
+const formSchema = z.object({
+  model: z.string().min(1, "Username must be at least 2 characters."),
+  temperature: z
+    .number()
+    .min(0, "Temperature must be bigger than 0")
+    .max(2, "Temperature must be less than 2"),
+  content: z
+    .string()
+    .min(50, "Content must be at least 50 characters.")
+    .max(500, "Content must be less than 500 characters."),
+  type: z.enum(["personal", "brand"], {
+    errorMap: () => ({ message: "Type is required" }),
+  }),
+  tone: z.enum(["friendly", "professional", "casual", "formal", "technical"], {
+    errorMap: () => ({ message: "Tone is required" }),
+  }),
+  emojis: z.boolean().default(false),
+});
+
+const UserInput = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      model: "llama3-8b-8192",
+      temperature: 1,
+      content: "",
+      type: "personal",
+      tone: "friendly",
+      emojis: false,
+    },
+  });
+
+  function onSubmit(data: z.infer<typeof formSchema>) {
+    console.log(data);
+  }
+
+  return (
+    <div className="bg-transparent border border-lime-500/10 shadow-inner shadow-lime-500/10 transition-all duration-300 p-4 rounded-lg">
+      <h2 className="text-3xl font-bold pb-2 decoration-dashed decoration-lime-500">User Input</h2>
+      <Separator className="my-4 border-lime-500/10 divide-dashed decoration-dashed dashed decoration-slice" />
+      <div className="relative flex flex-col gap-4">
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <fieldset className="grid gap-6 rounded-[8px] border border-lime-500/10 p-4 bg-lime-500/10">
+              <legend className="text-lg font-bold">Model Settings</legend>
+              <FormField
+                control={form.control}
+                name="model"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Model</FormLabel>
+                    <FormControl>
+                      <Input placeholder="shadcn" {...field} />
+                    </FormControl>
+                    <FormDescription>
+                      This is your public display name.
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </fieldset>
+            <Button type="submit">Submit</Button>
+          </form>
+        </Form>
+      </div>
+    </div>
+  );
+};
+
+export default UserInput;
