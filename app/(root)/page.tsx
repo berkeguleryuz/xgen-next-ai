a52ae5b42b20295fa0f85d4f31ee6714c09fc8a6
Berke Güleryüz <berkeguleryuz@gmail.com>
2025-03-07 13:08:49 +0100

    update

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,12 @@
 import HeroSection from "@/components/HeroSection";
 import PricingSection from "@/components/PricingSection";
-import { getProducts, getUser } from "@/utils/supabase/queries";
+import { getProducts } from "@/utils/supabase/queries";
 import { createClient } from "@/utils/supabase/server";
 
 
 export default async function Home() {
   const supabase = await createClient();
-  const [user, products] = await Promise.all([
-    getUser(supabase),
+  const [ products] = await Promise.all([
     getProducts(supabase),
   ]);
 
